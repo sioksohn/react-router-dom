732d5cdc0fc9e70d8cea5d869fb015717b707113
Elaine <sosohn@gmail.com>
2022-05-24 15:45:38 -0700

    Add useParams for dynamic pages

--- src/pages/QuoteDetail.tsx
+++ src/pages/QuoteDetail.tsx
@@ -1,5 +1,22 @@
+import { Fragment } from "react";
+import { useParams, Route } from "react-router-dom";
+import Comments from "../components/comments/Comments";
+
+type QuoteParam = {
+  quoteId: string;
+};
+
 const QuoteDetail = () => {
-  return <h1>Quote Detail Page</h1>;
+  const { quoteId } = useParams<QuoteParam>();
+  return (
+    <Fragment>
+      <h1>Quote Detail Page</h1>
+      <p>{quoteId}</p>
+      <Route path={`/quotes/${quoteId}/comments`}>
+        <Comments />
+      </Route>
+    </Fragment>
+  );
 };
 
 export default QuoteDetail;
