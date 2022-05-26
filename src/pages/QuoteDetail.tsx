4d3435b9a87b339c44303c052de04f11915cca97
Elaine <sosohn@gmail.com>
2022-05-25 21:47:04 -0700

    More flexible Routing with useRouteMatch

--- src/pages/QuoteDetail.tsx
+++ src/pages/QuoteDetail.tsx
@@ -1,5 +1,5 @@
 import { Fragment } from "react";
-import { useParams, Route, Link } from "react-router-dom";
+import { useParams, Route, Link, useRouteMatch } from "react-router-dom";
 
 import Comments from "../components/comments/Comments";
 import HighlightedQuote from "../components/quotes/HighlightedQuote";
@@ -10,6 +10,9 @@ type QuoteParam = {
 };
 
 const QuoteDetail = () => {
+  const match = useRouteMatch();
+  // console.log(match);
+  const { path, url } = match;
   const { quoteId } = useParams<QuoteParam>();
   const quote = DUMMY_QUOTES.find((quote) => quote.id === quoteId);
   if (!quote) {
@@ -18,14 +21,14 @@ const QuoteDetail = () => {
   return (
     <Fragment>
       <HighlightedQuote text={quote.text} author={quote.author} />
-      <Route path={`/quotes/${quoteId}`} exact>
+      <Route path={path} exact>
         <div className="centered">
-          <Link className="btn--flat" to={`/quotes/${quoteId}/comments`}>
+          <Link className="btn--flat" to={`${url}/comments`}>
             Load Comments
           </Link>
         </div>
       </Route>
-      <Route path={`/quotes/${quoteId}/comments`}>
+      <Route path={`${path}/comments`}>
         <Comments />
       </Route>
     </Fragment>
