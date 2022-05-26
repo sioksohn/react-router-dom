8a7f86e4c31d460700208ab1f5a294daa196089c
Elaine <sosohn@gmail.com>
2022-05-25 22:59:45 -0700

    Send data using useEffect in NewQuote

--- src/pages/NewQuote.tsx
+++ src/pages/NewQuote.tsx
@@ -1,3 +1,4 @@
+import { useEffect } from "react";
 import { useHistory } from "react-router-dom";
 import QuoteForm from "../components/quotes/QuoteForm";
 import { EnteredQuote } from "../components/quotes/QuoteForm";
@@ -5,8 +6,15 @@ import useHttp from "../hooks/use-http";
 import { addQuote } from "../lib/api";
 
 const NewQuote = () => {
-  const { sendRequest } = useHttp(addQuote);
+  const { sendRequest, status } = useHttp(addQuote);
   const history = useHistory();
+
+  useEffect(() => {
+    if (status === "completed") {
+      history.push("/quotes");
+    }
+  }, [status, history]);
+
   const addQuoteHandler = (quoteData: EnteredQuote) => {
     sendRequest(quoteData);
     history.push("/quotes");
