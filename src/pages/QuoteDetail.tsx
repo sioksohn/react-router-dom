879cf7db63ce523cf0688f5017a6e625aa9e6424
Elaine <sosohn@gmail.com>
2022-05-25 23:14:40 -0700

    Bring data from firebase on QuoteDetail

--- src/pages/QuoteDetail.tsx
+++ src/pages/QuoteDetail.tsx
@@ -1,8 +1,11 @@
-import { Fragment } from "react";
+import { Fragment, useEffect } from "react";
 import { useParams, Route, Link, useRouteMatch } from "react-router-dom";
 
 import Comments from "../components/comments/Comments";
 import HighlightedQuote from "../components/quotes/HighlightedQuote";
+import useHttp from "../hooks/use-http";
+import { getSingleQuote } from "../lib/api";
+import LoadingSpinner from "../components/UI/LoadingSpinner";
 
 type QuoteParam = {
   quoteId: string;
@@ -13,13 +16,37 @@ const QuoteDetail = () => {
   // console.log(match);
   const { path, url } = match;
   const { quoteId } = useParams<QuoteParam>();
-  const quote = DUMMY_QUOTES.find((quote) => quote.id === quoteId);
-  if (!quote) {
+
+  const {
+    sendRequest,
+    status,
+    data: loadedQuote,
+    error,
+  } = useHttp(getSingleQuote, true);
+
+  useEffect(() => {
+    sendRequest(quoteId);
+  }, [sendRequest, quoteId]);
+
+  if (status === "pending") {
+    return (
+      <div className="centered">
+        <LoadingSpinner />
+      </div>
+    );
+  }
+
+  if (error) {
+    return <p className="centered">{error}</p>;
+  }
+
+  if (!loadedQuote.text) {
     return <p>No quote found!</p>;
   }
+
   return (
     <Fragment>
-      <HighlightedQuote text={quote.text} author={quote.author} />
+      <HighlightedQuote text={loadedQuote.text} author={loadedQuote.author} />
       <Route path={path} exact>
         <div className="centered">
           <Link className="btn--flat" to={`${url}/comments`}>
